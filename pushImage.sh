d42b4e39303336cad13d08baf01bb328951d5e1e
Rafal <rafal.sladek@gmail.com>
2018-03-01 00:32:41 +0100

    added scripts for raspberry

--- pushImage.sh
+++ pushImage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "You should be logged in dockerhub before run this script"
-docker tag bitcoin-docker-base rafal.sladek/bitcoin-docker-base:latest
-docker push rafal.sladek/bitcoin-docker-base:latest
+docker tag bitcoin-docker-base:rsp rafal.sladek/bitcoin-docker-base:rsp
+docker push rafal.sladek/bitcoin-docker-base:rsp
